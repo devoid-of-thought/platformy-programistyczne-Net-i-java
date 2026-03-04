f926c505d1ffc311d11c17e7af255a6aea93a6ed
devoid-of-thought <mikynowy@gmail.com>
2026-03-04 18:19:55 +0100

    Dodano rozwiązania do laboratorium 1 fizzbuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,39 @@
+namespace solution
+{
+    class FizzBuzz
+    {
+        int number = 0;
+        public FizzBuzz(int number)
+        {
+            this.number = number;
+        }
+        public void Run()
+        {
+            for (int i = 0; i <= number; i++)
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+
+                    }
+                    else
+                        if (i % 3 == 0)
+                        {
+                            Console.WriteLine("Fizz");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(i);
+                        }
+
+        }
+
+    }
+}
+
+
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace solution
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Number:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            FizzBuzz fz = new FizzBuzz(number);
+
+            fz.Run();
+        }
+
+
+    }
+}
+
+
+
+
